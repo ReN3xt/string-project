2fe1afd20c6c8504af4215ea37931f9140e5ffbf
ReN3xt <isopi.marco@gmail.com>
2022-02-17 10:46:25 +0100

    Initial commit

---
+++ src/it/uniroma2/isopi/Main.java
@@ -0,0 +1,27 @@
+package it.uniroma2.isopi;
+
+public class Main {
+    String string = "";
+
+    public static void main(String[] args) {
+
+    }
+
+    public int calcolaNumeroVocali() {
+
+    }
+
+    public int calcolaNumeroConsonanti() {
+
+    }
+
+    public int calcolaNumeroSpazi() {
+
+    }
+
+    public int calcolaLettereDoppie() {
+
+    }
+
+
+}
